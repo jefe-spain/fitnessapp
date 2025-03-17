b6ae5fc34b262c5e960270eb858221ed157d0e44
Jesus Fernandez <jefe.1988@gmail.com>
2025-03-17 10:23:26 +0100

    remove custom module alias resolution from metro configuration

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,20 +1,9 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-/* eslint-env node */
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
 /** @type {import('expo/metro-config').MetroConfig} */
+// eslint-disable-next-line no-undef
 const config = getDefaultConfig(__dirname);
-
-// Add custom module alias resolution
-config.resolver = config.resolver || {};
-config.resolver.sourceExts = ['ts', 'tsx', 'js', 'jsx'];
-config.resolver.extraNodeModules = {
-  '@app': `${__dirname}/app`,
-  '@components': `${__dirname}/components`,
-  '@store': `${__dirname}/store`,
-  '@utilities': `${__dirname}/utilities`,
-  '@i18n': `${__dirname}/i18n`
-};
 
 module.exports = withNativeWind(config, { input: './global.css' });
